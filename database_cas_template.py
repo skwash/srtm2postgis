12a1ab733fdf25c357c14133d9a79834e4ba5cb4
Josh Hansen <josh@skwash.net>
2012-04-04 13:20:25 -0700

    creating cassandra config template following the previous method

---
+++ database_cas_template.py
@@ -0,0 +1,5 @@
+# For Cassandra, uncomment the lines below and fill in the correct values
+
+keyspace='SRTM'
+cf_name='AltitudeSRTM3'
+nodelist=['cas-001:9160', 'cas-002:9160', 'cas-005:9160', 'cas-006:9160', 'cas-009:9160']
